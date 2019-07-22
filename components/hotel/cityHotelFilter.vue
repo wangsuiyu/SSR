fbb7cab2ab09301bab075f1f77c9b5066b650573
Ada it <434979868@qq.com>
2019-07-22 23:52:57 +0800

    地图随地址更新

--- components/hotel/cityHotelFilter.vue
+++ components/hotel/cityHotelFilter.vue
@@ -227,15 +227,16 @@
 export default {
   data() {
     return {
-      id:0,
+      longitude:118.8718107,
+      latitude:31.32846821,
+      id:74,
       peoplenum:0,
       childnum:0,
       isHide:true,
       data:{},
       form: {
         city: '南京',
-        date: '',
-        id:0
+        date: ''
       },
       value: '',
       people: [{
@@ -342,14 +343,60 @@ export default {
                 });
                 }
             })
-    }
+    },
+    getMap(longitude,latitude){
+      // 页面加加载之后执行
+     window.onLoad = function () {
+      // 生成地图.container是显示地图的div的id
+      var map = new AMap.Map('map', {
+        zoom:9,//放大级别
+        center: [longitude, latitude],//中心点坐标，经纬度
+        viewMode: '3D'//使用3D视图
+      });
+
+      // 创建一个 Marker 实例：
+      var marker = new AMap.Marker({
+        //content: "<div style='width:20px; height:20px; background:red;'>1</div>",
+        position: new AMap.LngLat(longitude, latitude),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+        title: '北京'
+      });
+
+      map.add(marker);
+       }
+
+      // 地图的链接
+      var key = "fc349821166279b8365299d22d7cf202"
+      var url = `https://webapi.amap.com/maps?v=1.4.15&key=${key}&callback=onLoad`;
+      var jsapi = document.createElement('script');
+      jsapi.charset = 'utf-8';
+      jsapi.src = url;
+      document.head.appendChild(jsapi);
+      }
+  },
+ watch: {
+      id() {
+        this.$axios({
+        url:'hotels',
+        params:{
+          city:this.id
+        }
+      }).then(res=>{
+        const {data}=res.data
+        this.longitude=data[0].location.longitude
+        this.latitude=data[0].location.latitude
+        // console.log(1)
+        this.getMap(this.longitude,this.latitude)
+      })
+      }
+      
+  
   },
   mounted() {
     // 页面加加载之后执行
     window.onLoad = function () {
       // 生成地图.container是显示地图的div的id
       var map = new AMap.Map('map', {
-        zoom: 11,//放大级别
+        zoom: 9,//放大级别
         center: [118.8718107, 31.32846821],//中心点坐标，经纬度
         viewMode: '3D'//使用3D视图
       });
